c0991b356519d5314c6fc3b45a5a131d731a297c
Daler <dsarifkulo021@gmail.com>
2023-12-17 18:27:35 +0500

    footer created

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 interface FooterProps {
@@ -6,9 +8,19 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = () => {
     return (
-        <div>
-            Footer            
-        </div>
+        <footer className='border-t' >
+            <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row' >
+                <Link href="/" >
+                    <Image 
+                        src="/assets/images/logo.svg"
+                        alt="logo"
+                        width="128"
+                        height="38"
+                    />
+                </Link>
+                <p>2023 Evently. All rights reserved.</p>
+            </div>
+        </footer>
     );
 };
 
