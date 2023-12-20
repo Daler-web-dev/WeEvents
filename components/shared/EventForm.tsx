82d63597f9e3440434687ef4696c117693a196ca
Daler <dsarifkulo021@gmail.com>
2023-12-20 13:56:22 +0500

    create is work

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -16,6 +16,7 @@ import {
 	FormMessage,
 } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
+import {useUploadThing} from '@/lib/uploadthing'
 
 import { Input } from "@/components/ui/input";
 import { eventFormSchema } from "@/lib/validator";
@@ -24,6 +25,8 @@ import Dropdown from "./Dropdown";
 import { FileUploader } from "./FileUploader";
 import { useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { createEvent } from "@/lib/actions/event.actions";
 
 interface EventFormProps {
 	userId: string;
@@ -33,6 +36,9 @@ interface EventFormProps {
 const EventForm: React.FC<EventFormProps> = ({ userId, type }) => {
 	const [files, setFiles] = useState<File[]>([]);
 	const initialValues = eventDefaultValues;
+	const router = useRouter()
+
+	const {startUpload} = useUploadThing('imageUploader')
 
 	const form = useForm<z.infer<typeof eventFormSchema>>({
 		resolver: zodResolver(eventFormSchema),
@@ -40,10 +46,39 @@ const EventForm: React.FC<EventFormProps> = ({ userId, type }) => {
 	});
 
 	// 2. Define a submit handler.
-	function onSubmit(values: z.infer<typeof eventFormSchema>) {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
-		console.log(values);
+	async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+		const eventData = values
+
+		let uploadedImageUrl = values.imageUrl
+
+		if(files.length > 0) {
+			const uploadedImages = await startUpload(files)
+
+			if(!uploadedImages) {
+				return
+			}
+
+			uploadedImageUrl = uploadedImages[0].url
+		}
+
+		if(type === 'Create') {
+			try {
+				const newEvent = await createEvent({
+					event: { ...values, imageUrl: uploadedImageUrl},
+					userId,
+					path: "/profile"
+				})
+
+				if(newEvent) {
+					form.reset()
+					router.push(`/events/${newEvent._id}`)
+				}
+
+			} catch(error) {
+
+			}
+		}
+
 	}
 	return (
 		<Form {...form}>
